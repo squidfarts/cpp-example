a647f4e8a9b3045bd25ad2c057c4cf952d6ada8d
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:31:37 -0700

    Add files via upload

---
+++ src/main/main.cc
@@ -0,0 +1,29 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.cc                                                                   *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, c++' is desplayed to the standerd console.
+ *
+ */
+int main()
+{
+    std::cout << "Hello, c++." << std::endl;
+    
+    return EXIT_SUCCESS;
+}// end of function main
